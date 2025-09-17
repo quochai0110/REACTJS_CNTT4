7ead1c5caf811da50d395b98f28c3732800b111f
C4EJS100-TrinhQuocHai <trinhquochai12@gmail.com>
2025-09-17 15:01:03 +0700

    ss31

--- SESSION31/client/src/pages/ListPost.tsx
+++ SESSION31/client/src/pages/ListPost.tsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useEffect, type ChangeEvent } from "react";
 import { Table } from "react-bootstrap";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
+import axios from "axios";
 
+interface Post {
+  id: number;
+  title: string;
+  image: string;
+  date: string;
+  status: boolean;
+}
 export default function ListPost() {
-  const [show, setShow] = useState(false);
-
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [show, setShow] = useState<boolean>(false);
+  const[post,setPost]=useState<any>({
+    title:"",
+    image:"",
+    date:new Date().getDay,
+    status:true
+  })
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  async function getAllPost() {
+    try {
+      const reponse = await axios.get("http://localhost:8080/posts");
+      setPosts(reponse.data);
+    } catch (error) {
+      console.error("Loi API: ", error);
+    }
+  }
+
+  useEffect(() => {
+    getAllPost();
+  }, []);
+
+  const handleSave= async()=>{
+    const response= await axios.post("http://localhost:8080/posts",post);
+     setPosts([...posts,response.data]);
+     setShow(false);
+
+  }
+  const handleChange=(e:any)=>{
+    const {name,value}=e.target;
+    setPost({...post,[name]:value})
+  }
 
   return (
     <div>
       <h1> Danh sách bài viết</h1>
-      <input type="text" />
+      <input type="text" placeholder="nhập từ khóa tìm kiếm" />
       <select name="" id="">
         <option value="">Lựa chọn</option>
         <option value="">Đã xuất bản</option>
         <option value="">Chưa xuất bản</option>
       </select>
-     
+
       <Button variant="primary" onClick={handleShow}>
         Thêm bài viết
       </Button>
@@ -33,25 +71,19 @@ export default function ListPost() {
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Tên bài viết</Form.Label>
-              <Form.Control
-                type="email"
-                autoFocus
-              />
+              <Form.Control type="email" autoFocus onChange={handleChange} name="title" />
             </Form.Group>
-             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Hình ảnh</Form.Label>
-              <Form.Control
-                type="email"
-                autoFocus
-              />
+              <Form.Control type="text" autoFocus onChange={handleChange} name="image" />
             </Form.Group>
-           
+
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
               <Form.Label>Nội dung bài viết</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control as="textarea" rows={3} onChange={handleChange} name="content"/>
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -59,7 +91,7 @@ export default function ListPost() {
           <Button variant="secondary" onClick={handleClose}>
             Hủy
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleSave}>
             Lưu bài viết
           </Button>
         </Modal.Footer>
@@ -76,9 +108,22 @@ export default function ListPost() {
           </tr>
         </thead>
         <tbody>
-          <tr></tr>
-          <tr></tr>
-          <tr></tr>
+          {posts.map((item, index) => {
+            return (
+              <tr key={item.id}>
+                <td>{index + 1}</td>
+                <td>{item.title}</td>
+                <td>{item.image}</td>
+                <td>{item.date}</td>
+                <td>{item.status ? "Đã xuất bản" : "Chưa xuất bản"}</td>
+                <td>
+                  <Button variant="primary">Chặn</Button>
+                  <Button variant="secondary">Sửa</Button>
+                  <Button variant="success">Xóa</Button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
     </div>
